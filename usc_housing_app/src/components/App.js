7c6deeceee7547dfb34638d8388b68a5b07b5643
Richard Zhang <rrzhang@usc.edu>
2021-03-06 13:05:22 -0800

    barebones project

---
+++ usc_housing_app/src/components/App.js
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+import Home from './Home.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ usc_housing_app/src/components/Home.js
@@ -0,0 +1,13 @@
+import '../styles/Home.css';
+
+function Home() {
+    return (
+        <div className="Home">
+            <header className="Home-header">
+
+            </header>
+        </div>
+    );
+}
+
+export default Home;
